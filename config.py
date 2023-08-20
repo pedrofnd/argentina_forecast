8cec28a9c8dec072fbff433282d89095eb5ce158
Pedro Faria <pedro.faria@eneva.com.br>
2023-08-20 14:48:56 -0300

    config e forecast de forecast_ETS ajustados para o modo de funcao

--- config.py
+++ config.py
@@ -214,3 +214,132 @@ def test_residuals(residuals):
         print("Não há evidência de efeitos ARCH nos resíduos.")
     else:
         print("Há evidência de efeitos ARCH nos resíduos.")
+
+
+###------------------------------------------------------------###
+#------------- FUNCOES PARA O CODIGO forecast_ETS----------------#
+###------------------------------------------------------------###
+
+def plot_exponential_forecasts(train_data, test_data, predictions_dict, title):
+    plt.figure(figsize=(10, 6))
+    plt.plot(train_data.index, train_data['energia(mwmed)'], label='Treinamento')
+    plt.plot(test_data.index, test_data['energia(mwmed)'], label='Teste')
+    for method, predictions in predictions_dict.items():
+        plt.plot(test_data.index, predictions, label=f'Previsões {method}', linestyle='dashed')
+    plt.xlabel('Data')
+    plt.ylabel('Energia (mwmed)')
+    plt.title(title)
+    plt.legend()
+    plt.show()
+def plot_four_quadrants(data, title):
+    fig = plt.figure(figsize=(12, 8))
+    # Primeiro e segundo quadrantes - Série temporal completa
+    ax1 = fig.add_subplot(2, 2, (1, 2))
+    ax1.plot(data.index, data['energia(mwmed)'])
+    ax1.set_title('Série Temporal Completa')
+    ax1.set_xlabel('Data')
+    ax1.set_ylabel('Energia (mwmed)')
+    # Terceiro quadrante - Gráfico por mês e ano
+    ax2 = fig.add_subplot(2, 2, 3)
+    grouped_monthly = data.groupby([data.index.month, data.index.year]).sum()
+    grouped_monthly['energia(mwmed)'].unstack().plot(ax=ax2, marker='o')
+    ax2.set_title('Energia por Mês e Ano')
+    ax2.set_xlabel('Mês')
+    ax2.set_ylabel('Energia (mwmed)')
+    ax2.set_xticks(range(1, 13))
+    ax2.set_xticklabels(['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez'])
+    # Quarto quadrante - Histograma
+    ax3 = fig.add_subplot(2, 2, 4)
+    ax3.hist(data['energia(mwmed)'], bins=20)
+    ax3.set_title('Histograma')
+    ax3.set_xlabel('Energia (mwmed)')
+    ax3.set_ylabel('Frequência')
+    # Ajustar layout para evitar sobreposição
+    plt.tight_layout()
+    # Exibir os gráficos
+    plt.show()
+
+def mean_absolute_percentage_error(y_true, y_pred):
+    return np.mean(np.abs((y_true - y_pred) / y_true)) * 100
+
+
+def plot_ets_decomposition(ets_fit):
+    fig, axes = plt.subplots(4, 1, figsize=(10, 10))
+    decomposition = seasonal_decompose(ets_fit.fittedvalues, model='additive', period=12)
+    # Plotar a componente observada
+    decomposition.observed.plot(ax=axes[0], legend=False, color='blue')
+    axes[0].set_ylabel('Observado')
+    axes[0].set_title('Decomposição dos Dados - Observado')
+    # Plotar a componente de tendência
+    decomposition.trend.plot(ax=axes[1], legend=False, color='red')
+    axes[1].set_ylabel('Tendência')
+    axes[1].set_title('Decomposição dos Dados - Tendência')
+    # Plotar a componente sazonal
+    decomposition.seasonal.plot(ax=axes[2], legend=False, color='green')
+    axes[2].set_ylabel('Sazonalidade')
+    axes[2].set_title('Decomposição dos Dados - Sazonalidade')
+    # Plotar os resíduos
+    decomposition.resid.plot(ax=axes[3], legend=False, color='purple')
+    axes[3].set_ylabel('Resíduos')
+    axes[3].set_title('Decomposição dos Dados - Resíduos')
+    # Ajustar o layout para evitar sobreposição
+    plt.tight_layout()
+    plt.show()
+
+
+def plot_metrics_comparison(metrics_dict):
+    models = list(metrics_dict.keys())
+    rmse_values = [metrics_dict[model]['RMSE'] for model in models]
+    mae_values = [metrics_dict[model]['MAE'] for model in models]
+    mape_values = [metrics_dict[model]['MAPE'] for model in models]
+    fig, axs = plt.subplots(3, 1, figsize=(10, 15))
+    # Gráfico de barras para RMSE
+    axs[0].bar(models, rmse_values, color='blue')
+    axs[0].set_title('Comparação de RMSE entre Modelos')
+    axs[0].set_xlabel('Modelos')
+    axs[0].set_ylabel('RMSE')
+    # Gráfico de barras para MAE
+    axs[1].bar(models, mae_values, color='green')
+    axs[1].set_title('Comparação de MAE entre Modelos')
+    axs[1].set_xlabel('Modelos')
+    axs[1].set_ylabel('MAE')
+    # Gráfico de barras para MAPE
+    axs[2].bar(models, mape_values, color='orange')
+    axs[2].set_title('Comparação de MAPE entre Modelos')
+    axs[2].set_xlabel('Modelos')
+    axs[2].set_ylabel('MAPE')
+    # Ajustar layout
+    plt.tight_layout()
+    # Mostrar os gráficos
+    plt.show()
+
+def analyze_ets_residuals(test_data, ets_predictions):
+    # Calcular os resíduos
+    residuals_ets = test_data['energia(mwmed)'] - ets_predictions
+    # Plotar gráfico dos resíduos
+    plt.figure(figsize=(10, 4))
+    plt.plot(test_data.index, residuals_ets, label='Resíduos - ETS', color='green')
+    plt.axhline(y=0, color='gray', linestyle='dashed')
+    plt.xlabel('Data')
+    plt.ylabel('Resíduos')
+    plt.title('Resíduos do Modelo ETS')
+    plt.legend()
+    plt.show()
+    # Plotar histograma dos resíduos
+    plt.figure(figsize=(10, 6))
+    plt.hist(residuals_ets, bins=30, color='blue', edgecolor='black')
+    plt.title('Histograma dos Resíduos do Modelo ETS')
+    plt.xlabel('Resíduos')
+    plt.ylabel('Frequência')
+    plt.grid(True)
+    plt.show()
+    # Plotar a autocorrelação dos resíduos
+    plot_acf(residuals_ets, lags=5)
+    plt.xlabel('Lags')
+    plt.ylabel('Autocorrelação')
+    plt.title('Autocorrelação dos Resíduos - ETS')
+    plt.show()
+    # Realizar o teste de Ljung-Box para verificar a autocorrelação dos resíduos
+    lb_test_ets = acorr_ljungbox(residuals_ets, lags=5)
+    print("lb_test_ets:", lb_test_ets)
+
--- forecast_ETS.py
+++ forecast_ETS.py
@@ -1,8 +1,9 @@
 #objetivo eh utilizar as previsões estatisticas (primeiramente mensais) para determinar carga
 import os.path
 import pandas as pd
-from config import datai, dataf, arquivo1, arquivo2, path
-import pandas as pd
+from config import path, mean_absolute_percentage_error
+from config import plot_exponential_forecasts, plot_four_quadrants, plot_ets_decomposition, plot_metrics_comparison
+from config import analyze_ets_residuals
 import numpy as np
 from statsmodels.tsa.holtwinters import SimpleExpSmoothing, ExponentialSmoothing
 from sklearn.metrics import mean_squared_error
@@ -15,10 +16,9 @@ from sklearn.metrics import mean_absolute_error, mean_absolute_percentage_error
 from statsmodels.tsa.seasonal import seasonal_decompose
 
 
-
 ##path com o banco de dados em CSV com o formato extraido a partir do projeto da API-CAMMESA
 # path2 = r'BD_historico'
-path2 = ""
+path2 = "DB"
 DB = 'balanco_mensal.csv'
 path_complete = os.path.join(path,path2,DB)
 print(path_complete)
@@ -50,48 +50,6 @@ df_oferta_data.to_csv('db_oferta_groupdata.csv', index=False, sep= ';')
 #3)Suavização Exponencial de Holt-Winters (HW - Aditivo/Multiplicativo)
 #4)Modelo ETS (Error, Trend, Seasonal)
 
-def plot_four_quadrants(data, title):
-    fig = plt.figure(figsize=(12, 8))
-    # Primeiro e segundo quadrantes - Série temporal completa
-    ax1 = fig.add_subplot(2, 2, (1, 2))
-    ax1.plot(data.index, data['energia(mwmed)'])
-    ax1.set_title('Série Temporal Completa')
-    ax1.set_xlabel('Data')
-    ax1.set_ylabel('Energia (mwmed)')
-    # Terceiro quadrante - Gráfico por mês e ano
-    ax2 = fig.add_subplot(2, 2, 3)
-    grouped_monthly = data.groupby([data.index.month, data.index.year]).sum()
-    grouped_monthly['energia(mwmed)'].unstack().plot(ax=ax2, marker='o')
-    ax2.set_title('Energia por Mês e Ano')
-    ax2.set_xlabel('Mês')
-    ax2.set_ylabel('Energia (mwmed)')
-    ax2.set_xticks(range(1, 13))
-    ax2.set_xticklabels(['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez'])
-    # Quarto quadrante - Histograma
-    ax3 = fig.add_subplot(2, 2, 4)
-    ax3.hist(data['energia(mwmed)'], bins=20)
-    ax3.set_title('Histograma')
-    ax3.set_xlabel('Energia (mwmed)')
-    ax3.set_ylabel('Frequência')
-    # Ajustar layout para evitar sobreposição
-    plt.tight_layout()
-    # Exibir os gráficos
-    plt.show()
-
-def plot_exponential_forecasts(train_data, test_data, predictions_dict, title):
-    plt.figure(figsize=(10, 6))
-    plt.plot(train_data.index, train_data['energia(mwmed)'], label='Treinamento')
-    plt.plot(test_data.index, test_data['energia(mwmed)'], label='Teste')
-    for method, predictions in predictions_dict.items():
-        plt.plot(test_data.index, predictions, label=f'Previsões {method}', linestyle='dashed')
-    plt.xlabel('Data')
-    plt.ylabel('Energia (mwmed)')
-    plt.title(title)
-    plt.legend()
-    plt.show()
-
-def mean_absolute_percentage_error(y_true, y_pred):
-    return np.mean(np.abs((y_true - y_pred) / y_true)) * 100
 
 #converter para timeseries
 df_oferta_data['data'] = pd.to_datetime(df_oferta_data['data'])
@@ -188,30 +146,8 @@ metrics_dict['ETS'] = metrics_ets
 # 4) plotar os gráficos (usar funcao)
 plot_exponential_forecasts(train_data, test_data, {'SES': ses_predictions, 'SEH': seh_predictions, 'HW - Multiplicativo': hw_multiplicativo_predictions, 'ETS': ets_predictions}, 'Previsões de Energia usando Métodos Exponenciais')
 
-
-# Criar uma figura com quatro subplots e as componentes de decomposicao do ETS
-fig, axes = plt.subplots(4, 1, figsize=(10, 10))
-decomposition = seasonal_decompose(ets_fit.fittedvalues, model='additive', period=12)
-# Plotar a componente observada
-decomposition.observed.plot(ax=axes[0], legend=False, color='blue')
-axes[0].set_ylabel('Observado')
-axes[0].set_title('Decomposição dos Dados - Observado')
-# Plotar a componente de tendência
-decomposition.trend.plot(ax=axes[1], legend=False, color='red')
-axes[1].set_ylabel('Tendência')
-axes[1].set_title('Decomposição dos Dados - Tendência')
-# Plotar a componente sazonal
-decomposition.seasonal.plot(ax=axes[2], legend=False, color='green')
-axes[2].set_ylabel('Sazonalidade')
-axes[2].set_title('Decomposição dos Dados - Sazonalidade')
-# Plotar os resíduos
-decomposition.resid.plot(ax=axes[3], legend=False, color='purple')
-axes[3].set_ylabel('Resíduos')
-axes[3].set_title('Decomposição dos Dados - Resíduos')
-# Ajustar o layout para evitar sobreposição
-plt.tight_layout()
-plt.show()
-
+# Chamando a função com o objeto 'ets_fit'
+plot_ets_decomposition(ets_fit)
 
 # Criar o DataFrame a partir do dicionário de métricas e plotar grafico:
 metrics_df = pd.DataFrame(metrics_dict)
@@ -221,59 +157,12 @@ rmse_values = [metrics_dict[model]['RMSE'] for model in metrics_dict]
 mae_values = [metrics_dict[model]['MAE'] for model in metrics_dict]
 mape_values = [metrics_dict[model]['MAPE'] for model in metrics_dict]
 
-# ----- Modelos para o eixo x: plotagem dos parametros de erro -----
-models = list(metrics_dict.keys())
-# Criar subplots
-fig, axs = plt.subplots(3, 1, figsize=(10, 15))
-# Gráfico de barras para RMSE
-axs[0].bar(models, rmse_values, color='blue')
-axs[0].set_title('Comparação de RMSE entre Modelos')
-axs[0].set_xlabel('Modelos')
-axs[0].set_ylabel('RMSE')
-# Gráfico de barras para MAE
-axs[1].bar(models, mae_values, color='green')
-axs[1].set_title('Comparação de MAE entre Modelos')
-axs[1].set_xlabel('Modelos')
-axs[1].set_ylabel('MAE')
-# Gráfico de barras para MAPE
-axs[2].bar(models, mape_values, color='orange')
-axs[2].set_title('Comparação de MAPE entre Modelos')
-axs[2].set_xlabel('Modelos')
-axs[2].set_ylabel('MAPE')
-# Ajustar layout
-plt.tight_layout()
-# Mostrar os gráficos
-plt.show()
+# Chamando a função com o dicionário de métricas
+plot_metrics_comparison(metrics_dict)
 
 
-#----- testagem da previsao ETS:-----
-# Plotar resíduos - ETS
-residuals_ets = test_data['energia(mwmed)'] - ets_predictions
-plt.figure(figsize=(10, 4))
-plt.plot(test_data.index, residuals_ets, label='Resíduos - ETS', color='green')
-plt.axhline(y=0, color='gray', linestyle='dashed')
-plt.xlabel('Data')
-plt.ylabel('Resíduos')
-plt.title('Resíduos do Modelo ETS')
-plt.legend()
-plt.show()
-# Plotar um histograma dos resíduos
-plt.figure(figsize=(10, 6))
-plt.hist(residuals_ets, bins=30, color='blue', edgecolor='black')
-plt.title('Histograma dos Resíduos do Modelo ETS')
-plt.xlabel('Resíduos')
-plt.ylabel('Frequência')
-plt.grid(True)
-plt.show()
-# Plotar Avaliar autocorrelação dos resíduos - ETS
-plot_acf(residuals_ets, lags=5)
-plt.xlabel('Lags')
-plt.ylabel('Autocorrelação')
-plt.title('Autocorrelação dos Resíduos - ETS')
-plt.show()
-# Teste de Ljung-Box para verificar autocorrelação dos resíduos - ETS
-lb_test_ets = acorr_ljungbox(residuals_ets, lags=5)
-print("lb_test_ets:", lb_test_ets)
+# Chamando a função com os dados de teste e previsões do modelo ETS
+analyze_ets_residuals(test_data, ets_predictions)
 
 ##problema de convergencia !!!!
 ##modelo ETS: A inclusão do parâmetro 'damping_trend'
